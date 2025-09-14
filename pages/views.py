9879b7c1f2d4db7cfd588da5158febc205fb658b
Arvi-1nd <davidarvind44@gmail.com>
2025-09-14 21:39:27 +0530

    "added new tests in pages app"

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def homePageView(request):
-    return HttpResponse("hello world!")
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+    
+    
+    
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
